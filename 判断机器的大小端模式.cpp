ab598fc56d28eb3e959aa1dc5a9e815bf0a0be79
yuzhe <953659912@qq.com>
2016-09-24 15:39:20 +0800

    9_24

--- 判断机器的大小端模式.cpp
+++ 判断机器的大小端模式.cpp
@@ -21,9 +21,35 @@ void test1()
 
 }
 
+
+
+void test2()
+{
+	union A
+	{
+		int i;
+		char b;
+
+	}a;
+	a.i = 2;
+	if (a.b == 2)
+	{
+		printf("小端模式\n");
+	}
+	else
+	{
+		printf("大端模式\n");
+	}
+
+
+}
+
+
 int main()
 {
+
 	test1();
+	test2();
 	system("pause");
 	return 0;
 }
